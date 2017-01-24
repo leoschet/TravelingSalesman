957150875efee0ec07ed86ca2b9d1d643eac1f49
Leonardo Jose  Schettini de Arruda <a1601323@unet.univie.ac.at>
2017-01-24 13:50:22 +0100

    Integrate algorithm's webservice with chatbot

--- src/bobsalesman/restservices/Bob.java
+++ src/bobsalesman/restservices/Bob.java
@@ -23,28 +23,29 @@ public class Bob {
 	}
 	
 	@POST
-	@Produces(MediaType.TEXT_PLAIN)
-	@Path("/reqBestRoute")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/requestRoute")
 	public Response requestBestRoute(String url) throws IOException {
 		File file = bobsMarshall.getFile(url);
-		bobsFunctions.requestBestRoute(file);
-		return Response.ok("Funcionou", MediaType.TEXT_PLAIN).build();
+		int id = bobsFunctions.requestBestRoute(file);
+		return Response.ok("{ \"executionID\": " + id + " }", MediaType.APPLICATION_JSON).build();
 	}
 
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
-	@Path("/getBestRoute")
+	@Path("/download")
 	public Response getBestRoute(@QueryParam("key") int key) {
-		String url = bobsFunctions.getBestRoute(key);
-		return Response.ok(url, MediaType.TEXT_PLAIN).build();
+		String rawData = bobsFunctions.getBestRoute(key);
+		return Response.ok(rawData, MediaType.TEXT_PLAIN).build();
 	}
 	
 	
 	@GET
-	@Produces(MediaType.TEXT_PLAIN)
+	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/getProgress")
 	public Response getProgress(@QueryParam("key") int key) {
 		double progress = bobsFunctions.getProgress(key);
-		return Response.ok(progress, MediaType.TEXT_PLAIN).build();
+		return Response.ok("{ \"progress\": " + progress + " }", MediaType.APPLICATION_JSON).build();
 	}
+	
 }
